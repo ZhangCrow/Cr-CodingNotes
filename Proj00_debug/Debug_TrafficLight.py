5fdcf2ac32ad8dfb6dc887c60a3f4d81dadf071e
krzhang <>
2020-04-16 09:50:00 +0800

    update: testing pins

--- Proj00_debug/Debug_TrafficLight.py
+++ Proj00_debug/Debug_TrafficLight.py
@@ -3,33 +3,34 @@ import music
 
 in_traffic_light_loop = True
 
-# 交通灯模块 R接S2 Y接S1 G接S0 GND接G0
+# 交通灯模块 R接S14 Y接S13 G接S12 GND接G12
+# 土壤湿度检测器 S接S1 V接V1 G接G1
 # Traffic Light Module Loop
 while in_traffic_light_loop:
     music.play(music.JUMP_UP)
     display.clear()
     # 读取并展示土壤湿度
-    humidity = pin3.read_analog()
+    humidity = pin1.read_analog()
     display.show(humidity)
     # 初始化交通灯
-    pin0.write_digital(0)
-    pin1.write_digital(0)
-    pin2.write_digital(0)
+    pin12.write_digital(0)
+    pin13.write_digital(0)
+    pin14.write_digital(0)
     sleep(1000)
     # 绿灯亮
-    pin0.write_digital(1)
-    pin1.write_digital(0)
-    pin2.write_digital(0)
+    pin12.write_digital(1)
+    pin13.write_digital(0)
+    pin14.write_digital(0)
     sleep(2000)
     # 黄灯亮
-    pin0.write_digital(0)
-    pin1.write_digital(1)
-    pin2.write_digital(0)
+    pin12.write_digital(0)
+    pin13.write_digital(1)
+    pin14.write_digital(0)
     sleep(2000)
     # 红灯亮
-    pin0.write_digital(0)
-    pin1.write_digital(0)
-    pin2.write_digital(1)
+    pin12.write_digital(0)
+    pin13.write_digital(0)
+    pin14.write_digital(1)
     sleep(2000)
 
 # The end
