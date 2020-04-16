d24200b1f41fe7c9c81103d79a9cbdd8b5287439
krzhang <>
2020-04-16 10:45:17 +0800

    update: comments

--- Proj00_debug/Debug_TrafficLight.py
+++ Proj00_debug/Debug_TrafficLight.py
@@ -3,14 +3,37 @@ import music
 
 in_traffic_light_loop = True
 
-# 交通灯模块 R接S14 Y接S13 G接S12 GND接G12
-# 土壤湿度检测器 S接S1 V接V1 G接G1
+'''
+该文件用于
+1. 确保多个sensor可以同时work, 而不涉及具体的业务逻辑
+2. 尝试进行"Week 3 - Python and MicroBit Functions"中的Functions Definition and call
+'''
+
+'''
+极限情况下引脚需求：土壤湿度传感器1个，交通灯3个，水泵1个，水流传感器1个，环境光传感器1个
+
+根据以下2个文档
+https://tech.microbit.org/hardware/edgeconnector/
+https://microbit-micropython.readthedocs.io/en/latest/pin.html
+得知pins 5, 11, 3, 4, 6, 7, 9, 10, 19 and 20 已经被预设用于按钮或LED或I2C
+所以可用pins 0, 1, 2, 8, 12, 13, 14, 15, 16 共9个引脚
+
+根据MakeCode editor 中的提示
+https://makecode.microbit.org/#editor
+得知 pins 0, 1, 2, 3, 4, 10 attribute 支持 readwrite
+其他pins attribute 只支持 writeOnly
+
+所以
+交通灯模块 GND接G12 G接S12 Y接S13 R接S14
+土壤湿度检测器 G接G0 V接V0 S接S0
+'''
+
 # Traffic Light Module Loop
 while in_traffic_light_loop:
     music.play(music.JUMP_UP)
     display.clear()
     # 读取并展示土壤湿度
-    humidity = pin1.read_analog()
+    humidity = pin0.read_analog()
     display.show(humidity)
     # 初始化交通灯
     pin12.write_digital(0)
